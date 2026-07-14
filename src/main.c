081bea262ceeea6da9abdcdf730a58df3de552ff
xblink <talavarcell@gmail.com>
2026-07-14 14:13:53 -0400

    basic ps/2 keyboard support

--- src/main.c
+++ src/main.c
@@ -30,6 +30,22 @@ static volatile uint64_t limine_requests_start_marker[] = LIMINE_REQUESTS_START_
 __attribute__((used, section(".limine_requests_end")))
 static volatile uint64_t limine_requests_end_marker[] = LIMINE_REQUESTS_END_MARKER;
 
+
+
+static inline uint8_t inb(uint16_t port)
+{
+    uint8_t value;
+    asm volatile ("inb %1, %0"
+                  : "=a"(value)
+                  : "Nd"(port));
+    return value;
+}
+
+static inline void io_wait(void)
+{
+    asm volatile ("outb %%al, $0x80" : : "a"(0));
+}
+
 // GCC and Clang reserve the right to generate calls to the following
 // 4 functions even if they are not directly called.
 // Implement them as the C specification mandates.
@@ -96,6 +112,71 @@ static void hcf(void) {
 
 static size_t cursor_x = 10;
 static size_t cursor_y = 10;
+
+static const char scancode_to_ascii[128] = {
+    [0x02] = '1',
+    [0x03] = '2',
+    [0x04] = '3',
+    [0x05] = '4',
+    [0x06] = '5',
+    [0x07] = '6',
+    [0x08] = '7',
+    [0x09] = '8',
+    [0x0A] = '9',
+    [0x0B] = '0',
+
+    [0x0C] = '-',
+    [0x0D] = '=',
+
+    [0x0E] = '\b',
+    [0x0F] = '\t',
+
+    [0x10] = 'q',
+    [0x11] = 'w',
+    [0x12] = 'e',
+    [0x13] = 'r',
+    [0x14] = 't',
+    [0x15] = 'y',
+    [0x16] = 'u',
+    [0x17] = 'i',
+    [0x18] = 'o',
+    [0x19] = 'p',
+
+    [0x1A] = '[',
+    [0x1B] = ']',
+
+    [0x1C] = '\n',
+
+    [0x1E] = 'a',
+    [0x1F] = 's',
+    [0x20] = 'd',
+    [0x21] = 'f',
+    [0x22] = 'g',
+    [0x23] = 'h',
+    [0x24] = 'j',
+    [0x25] = 'k',
+    [0x26] = 'l',
+
+    [0x27] = ';',
+    [0x28] = '\'',
+    [0x29] = '`',
+
+    [0x2B] = '\\',
+
+    [0x2C] = 'z',
+    [0x2D] = 'x',
+    [0x2E] = 'c',
+    [0x2F] = 'v',
+    [0x30] = 'b',
+    [0x31] = 'n',
+    [0x32] = 'm',
+
+    [0x33] = ',',
+    [0x34] = '.',
+    [0x35] = '/',
+
+    [0x39] = ' ',
+};
 
 static const uint8_t font[128][7] = {
 
@@ -1057,49 +1138,84 @@ void draw_char(struct limine_framebuffer *fb, char c, size_t x, size_t y)
         }
     }
 }
-
-
-void print(struct limine_framebuffer *fb, const char *str, size_t x, size_t y)
+void putchar(struct limine_framebuffer *fb, char c)
 {
-    size_t start_x = x;
+    draw_char(fb, c, cursor_x, cursor_y);
 
+    cursor_x += 6;
+
+    if (cursor_x >= fb->width - 6)
+    {
+        cursor_x = 10;
+        cursor_y += 8;
+    }
+}
+
+void print(struct limine_framebuffer *fb, const char *str)
+{
     while (*str)
     {
         if (*str == '\n')
         {
-            x = start_x;
-            y += 8; // 7 pixel font + 1 pixel spacing
+            cursor_x = 10;
+            cursor_y += 8;
         }
         else
         {
-            draw_char(fb, *str, x, y);
-            x += 6;
+            putchar(fb, *str);
         }
 
         str++;
     }
 }
+char keyboard_getchar(void)
+{
+    for (;;)
+    {
+        // Wait until the keyboard controller has data.
+        while (!(inb(0x64) & 1))
+            ;
 
+        uint8_t scancode = inb(0x60);
 
+        // Ignore key releases.
+        if (scancode & 0x80)
+            continue;
+
+        char c = scancode_to_ascii[scancode];
+
+        if (c != 0)
+            return c;
+    }
+}
 
 // The following will be our kernel's entry point.
 // If renaming kmain() to something else, make sure to change the
 // linker script accordingly.
 void kmain(void)
 {
-    if (LIMINE_BASE_REVISION_SUPPORTED(limine_base_revision) == false) {
+    if (LIMINE_BASE_REVISION_SUPPORTED(limine_base_revision) == false)
+    {
         hcf();
     }
 
     if (framebuffer_request.response == NULL ||
-        framebuffer_request.response->framebuffer_count < 1) {
+        framebuffer_request.response->framebuffer_count < 1)
+    {
         hcf();
     }
 
     struct limine_framebuffer *framebuffer =
         framebuffer_request.response->framebuffers[0];
 
-    print(framebuffer, " ____  _  _______\n|  _ \| |/ /_   _|\n| |_) | ' /  | |\n|  _ <| . \  | |\n|_| \_\_|\_\ |_|\n-----------------\nOS: RKT\nKernel: RKT 0.1\nCPU: some sort of 64-bit cpu\nRAM: idk\nShell: nonexistent right now", 10, 10);
+    print(framebuffer,
+        " ____  _  _______\n|  _ \\| |/ /_   _|\n| |_) | ' /  | |\n|  _ <| . \\  | |\n|_| \\_\\_|\\_\\ |_|\n-----------------\nOS: RKT\nKernel: RKT 0.1\nCPU: some sort of 64-bit cpu\nRAM: idk\nShell: nonexistent right now\n\n"
+    );
+
+    while (1)
+    {
+        putchar(framebuffer, keyboard_getchar());
+    }
 
     hcf();
 }
